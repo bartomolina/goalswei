28319bca5d763ffdfd9abea50dac353178097af4
Barto Molina <bartomolina@gmail.com>
2022-12-29 16:38:37 +0100

    Added layout

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -1,8 +1,18 @@
+const { fontFamily } = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['var(--font-montserrat)', ...fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 }

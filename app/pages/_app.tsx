f6587eb198006a85ef42928e486ef0d6c4b1a54e
Barto Molina <bartomolina@gmail.com>
2022-12-31 12:35:19 +0100

    Added wagmi/ethers

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -1,11 +1,33 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "../components/layout";
+import { WagmiConfig, createClient, configureChains } from "wagmi";
+import { hardhat } from "wagmi/chains";
+import { jsonRpcProvider } from "wagmi/providers/jsonRpc";
+
+const { chains, provider, webSocketProvider } = configureChains(
+  [hardhat],
+  [
+    jsonRpcProvider({
+      rpc: (chain) => ({
+        http: `http://localhost:8545`,
+      }),
+    }),
+  ]
+);
+
+const client = createClient({
+  autoConnect: true,
+  provider,
+  webSocketProvider,
+});
 
 const App = ({ Component, pageProps }: AppProps) => (
-  <Layout>
-    <Component {...pageProps} />
-  </Layout>
+  <WagmiConfig client={client}>
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  </WagmiConfig>
 );
 
 export default App;
--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -1,26 +1,36 @@
 import Head from "next/head";
 import NewResolutionForm from "../components/new-resolution-form";
+import { useAccount, useConnect, useEnsName } from "wagmi";
+import { InjectedConnector } from "wagmi/connectors/injected";
 
-const Home = () => (
-  <>
-    <Head>
-      <title>WAGMI - Home</title>
-      <meta name="description" content="WAGMI" />
-    </Head>
-    <div className="mx-auto max-w-5xl sm:px-6 lg:px-8">
-      <div className="px-4 py-8 sm:px-0">
-        <header>
-          <h1 className="text-3xl font-semibold space-y-3">
-            <span className="block">🥇 Achieve your goals.</span>
-            <span className="block">🤗 Support awesome devs.</span>
-          </h1>
-        </header>
-        <main className="pt-10">
-          <NewResolutionForm />
-        </main>
+const Home = () => {
+  const { address, isConnected } = useAccount();
+  const { connect } = useConnect({
+    connector: new InjectedConnector(),
+  });
+
+  return (
+    <>
+      <Head>
+        <title>WAGMI - Home</title>
+        <meta name="description" content="WAGMI" />
+      </Head>
+      <div className="mx-auto max-w-5xl sm:px-6 lg:px-8">
+        <div className="px-4 py-8 sm:px-0">
+          <header>
+            <h1 className="text-3xl font-semibold space-y-3">
+              <span className="block">🥇 Achieve your goals.</span>
+              <span className="block">🤗 Support awesome devs.</span>
+            </h1>
+            <div>Test: { address }</div>
+          </header>
+          <main className="pt-10">
+            <NewResolutionForm />
+          </main>
+        </div>
       </div>
-    </div>
-  </>
-);
+    </>
+  );
+};
 
 export default Home;
